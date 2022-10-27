a48bd03abeef1ea35238d749dda1e788e318beff
Denis <denismachado@edu.campobelo.mg.gov.br>
2022-10-27 11:04:37 -0300

    questão3

---
+++ TrabalhoPoo3/src/round3/Abracadabra.java
@@ -0,0 +1,17 @@
+package round3;
+
+public class Abracadabra {
+
+	public static void main(String[] args) {
+		int A[]= {1,2,3,4,5,6,7,8,9,10};
+		int result=0;
+		for(int i=0;i < A.length;i++) {
+			result+=A[i];
+			
+		}
+		System.out.println("Soma dos valores do vetor:"+result);
+		
+
+	}
+
+}
